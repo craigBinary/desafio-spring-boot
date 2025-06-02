70ff8abf87d436c9733443a238d1cda1c270d1cf
craigBinary <cfcraig64@gmail.com>
2025-06-02 18:23:48 -0400

    refactor: limpieza de archivos compilados y binarios

--- taskmanagementapi/target/classes/import.sql
+++
@@ -1,7 +0,0 @@
-insert into usuarios (nombre_usuario, clave, correo) values ('Craig Fernandez', '$2a$10$85UgrNtACOImrNuIXtxY9ucmvKHJSTX8jRQ6EUXCtSfXTx.bhmVOm', 'craig_fernandez@gmail.com');
-insert into usuarios (nombre_usuario, clave, correo) values ('Rodrigo Mercado', '$2a$10$85UgrNtACOImrNuIXtxY9ucmvKHJSTX8jRQ6EUXCtSfXTx.bhmVOm', 'rodrigo_mercado@gmail.com');
-insert into usuarios (nombre_usuario, clave, correo) values ('Cristian Hernandez', '$2a$10$85UgrNtACOImrNuIXtxY9ucmvKHJSTX8jRQ6EUXCtSfXTx.bhmVOm', 'cristian_hernandez@gmail.com');
-
-insert into estado_tarea (nombre_estado) values ('pendiente');
-insert into estado_tarea (nombre_estado) values ('en progreso');
-insert into estado_tarea (nombre_estado) values ('finalizada');
